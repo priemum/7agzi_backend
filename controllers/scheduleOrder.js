5afac2fb19471a89cb867e65bc74155bd8aa3ddb
Ahmed Hamouda <ahmedabdelrazzak1001010@gmail.com>
2023-06-20 21:33:11 -0700

    Added

--- controllers/scheduleOrder.js
+++ controllers/scheduleOrder.js
@@ -95,7 +95,6 @@ exports.create = (req, res) => {
 		orderStatusSMS.messages
 			.create({
 				from: "whatsapp:+19512591528",
-				body: `Hi {{1}} - Your appointment was scheduled at ({{2}}) on {{3}}. Please check your dashboard or call us at {{4}} in case you would like to make any changes. Thank you for choosing {{5}}.`,
 				template: "appointment_confirmation",
 				appointment_confirmation: {
 					1: order.scheduledByUserName,
